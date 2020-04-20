b1f0f8b80591e28a9f565cdb6c690084de87d011
arturmus <artur.musaelyan@gmail.com>
2020-04-20 15:37:11 +0300

    discount

--- product/inex.php
+++ product/inex.php
@@ -2,21 +2,54 @@
 <head></head>
     <body>
     <h1>
-discount
+PRODUCT
 </h1>
-    <p>
+   
     <?php
             $product = $_GET['product'];
             $price = $_GET['price'];
             $quantity = $_GET['quantity'];
-            $total = $price * $quantity;
             $discount = $_GET['discount'];
+            $total = $price * $quantity - $price * $quantity * $discount / 100;
+
+        if ($discount !== '') {
+    $discount = 0;
+}
+
+
+        if (!is_numeric ($price) || !is_numeric ($quantity) || !is_numeric ($discount)) {
+    
+    echo 'Unexpected operation';
+    exit;
+}
+        if (($price <= 0) || ($quantity <=0) || ($discount < 0)) {
+    echo 'Unexpected operation';
+    exit;
+}
+                
 
         echo "$product $price $quantity $total";
 
 ?>
+<table border>
+    <tr>  
+        <th>Product</th>
+        <th>Price</th>
+        <th>Quantity</th>
+        <th>Discont</th>
+        <th>Total</th>
+    </tr>
 
-    </p>
+        <td><?php echo $product; ?></td>    
+        <td><?php echo $price; ?></td>
+        <td><?php echo $quantity; ?></td>
+        <td><?php echo $discount, '%'; ?></td>
+        <td><?php echo $total; ?></td>
+
+
+
+</table>
+    
     </body>
 
 </html>
