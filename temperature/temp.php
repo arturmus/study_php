0226f8d237f28bb5650f83a23907f494f99186db
arturmus <artur.musaelyan@gmail.com>
2020-05-13 17:12:59 +0300

    temperature

--- temperature/temp.php
+++ temperature/temp.php
@@ -1,14 +1,13 @@
 <?php
-        $a = $_POST['temp'];
-        $operation = isset($_POST['operation']);
-        $converte = $_POST['converte'] ?? 'undefined';
-    if ($operation === 'F') {
-        $convert = (($a - 32) / 1.8);
-        
+$a = $_POST['temp'];
+$operation = $_POST['operation'] ?? 'undefined';
+
+if ($operation === 'F') {
+        $convert = ($a - 32) / 1.8;   
 }
 
-    if ($operation === 'C') {
-        $convert = ($a * 1.8 - 32);
-       
+if ($operation === 'C') {
+        $convert = ($a * 1.8 - 32);  
 }
- echo $convert;
+
+echo $convert;
